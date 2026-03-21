bc33f12221e83632ca32e0d030522a0888f172f3
Maksim Kosaretskiy <ma.ko1995@mail.ru>
2026-03-21 20:14:54 +0300

    'blogicum'

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -13,13 +13,15 @@ import os
 from dotenv import load_dotenv
 
 from pathlib import Path
+
 from django.urls import reverse_lazy
+
 
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
